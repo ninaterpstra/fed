c68b15681b83f991345390468bc03f0de1edee2f
ninaterpstra <144121785+ninaterpstra@users.noreply.github.com>
2023-09-25 11:17:32 +0200

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,13 @@
-// JavaScript Document
-console.log("hi");
+const hamburger_menu = document.querySelector(".hamburger_menu");
+const menu_nav = document.querySelector(".menu_nav");
+
+hamburger_menu.addEventListener("click", () => {
+    hamburger_menu.classList.toggle("active");
+    menu_nav.classList.toggle("active");
+})
+
+document.querySelectorAll(".link_nav").forEach(n => n.
+    addEventListener("click", () => {
+        hamburger_menu.classList.remove("active");
+        menu_nav.classList.remove("active");
+    }))
